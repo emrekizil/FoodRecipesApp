af01ebd9710a9b024a75a0325943882b0ac40ad8
emrekizil <95411289+emrekizil@users.noreply.github.com>
2023-03-02 23:52:39 +0300

    added instructions fragment and webview

--- app/src/main/java/com/example/foodrecipesapp/ui/fragments/detail/InstructionsFragment.kt
+++ app/src/main/java/com/example/foodrecipesapp/ui/fragments/detail/InstructionsFragment.kt
@@ -5,18 +5,37 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.foodrecipesapp.R
+import android.webkit.WebViewClient
+import com.example.foodrecipesapp.data.dto.Result
+import com.example.foodrecipesapp.databinding.FragmentInstructionsBinding
 
 
 class InstructionsFragment : Fragment() {
 
-
+    private lateinit var binding:FragmentInstructionsBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_instructions, container, false)
+        binding = FragmentInstructionsBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupWebView()
+    }
+
+    private fun setupWebView() {
+        val args = arguments
+        val bundle:Result? = args?.getParcelable("recipeBundle")
+
+        binding.webView.webViewClient = object : WebViewClient() {}
+        val webUrl: String? = bundle?.sourceUrl
+        if (webUrl != null) {
+            binding.webView.loadUrl(webUrl)
+        }
+
     }
 
 }
